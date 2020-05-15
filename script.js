fd2a8949e57914a8d4669f5a61f5a0dee71b33b7
Jonathan <jonte.backhed@gmail.com>
2020-05-16 00:51:10 +0200

    added delay on button

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 window.addEventListener('load', () => {
     let input = document.querySelector('#country');
     let button = document.querySelector('#button');
+    let onDelay = false;
 
-    // addOptionsToHTML();
+    addOptionsToHTML();
+    getGlobalStatistics();
 
     button.addEventListener('click', () => {
-        // getCountrySpecificInfo(input.value);
+        if (onDelay == false) {
+            getCountrySpecificInfo(input.value);
 
-        for (let i = 0; i < 5; i++) {
-            delay(i);
+            onDelay = true;
+            for (let i = 0; i < 5; i++) {
+                delay(i);
+            }
+            setTimeout(() => {
+                button.innerText = "Get info";
+                onDelay = false;
+            }, 5000);
+        } else {
+            alert("Function is on cooldown");
         }
-        
-        button.innerText = "Get info";
-        console.log("back");
     })
 })
 
 function delay(i) {
     setTimeout(() => {
         button.innerText = i+1;
-        console.log(i+1);
     }, 1000 * i);
 }
+
 function addOptionsToHTML() {
     let url = "https://api.covid19api.com/summary";
     let selectBox = document.querySelector('#country');
@@ -57,28 +65,28 @@ function getCountrySpecificInfo(userInp) {
     })
 }
 
-// function getGlobalStatistics() {
-//     let url = "https://api.covid19api.com/summary";
-//     let globalCases = document.querySelector('#totalGlobalCases');
-//     let globalDeaths = document.querySelector('#totalGlobalDeaths');
-//     let globalRecoveries = document.querySelector('#totalGlobalRecoveries');
+function getGlobalStatistics() {
+    let url = "https://api.covid19api.com/summary";
+    let globalCases = document.querySelector('#totalGlobalCases');
+    let globalDeaths = document.querySelector('#totalGlobalDeaths');
+    let globalRecoveries = document.querySelector('#totalGlobalRecoveries');
 
-//     fetch(url)
-//     .then(response => response.json())
-//     .then(json => {
-//         globalCases.innerText = json.Global.TotalConfirmed;
-//         globalDeaths.innerText = json.Global.TotalDeaths;
-//         globalRecoveries.innerText = json.Global.TotalRecovered;
-//     })
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        globalCases.innerText = json.Global.TotalConfirmed;
+        globalDeaths.innerText = json.Global.TotalDeaths;
+        globalRecoveries.innerText = json.Global.TotalRecovered;
+    })
 
-//     Updates the header every 10 minutes
-//     setInterval(function (){
-//         fetch(url)
-//         .then(response => response.json())
-//         .then(json => {
-//             globalCases.innerText = json.Global.TotalConfirmed;
-//             globalDeaths.innerText = json.Global.TotalDeaths;
-//             globalRecoveries.innerText = json.Global.TotalRecovered;
-//         })
-//     }, 600000)
-// }
+    // Updates the header every 10 minutes
+    setInterval(function (){
+        fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            globalCases.innerText = json.Global.TotalConfirmed;
+            globalDeaths.innerText = json.Global.TotalDeaths;
+            globalRecoveries.innerText = json.Global.TotalRecovered;
+        })
+    }, 600000)
+}
